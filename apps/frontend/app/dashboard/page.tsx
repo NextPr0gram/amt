0113c19b4335c2ef3d1066647a0e68e2900bb530
NextPr0gram <anafkarim@proton.me>
2025-01-19 10:41:55 +0000

    feat(frontend): add separator in date header

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -10,9 +10,7 @@ export default function Page() {
             <AppSidebar />
             <SidebarInset>
                 {<DateStage />}
-                <div className="px-4">
-                    <Separator />
-                </div>
+                <div className="px-4"></div>
                 <header className="flex h-16 shrink-0 items-center gap-2">
                     <div className="flex items-center gap-2 px-4">
                         <Breadcrumb>
--- apps/frontend/components/date-stage.tsx
+++ apps/frontend/components/date-stage.tsx
@@ -1,4 +1,4 @@
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "./ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export function DateStage() {
@@ -8,6 +8,7 @@ export function DateStage() {
             <div className="flex h-16 shrink-0 items-center gap-2">
                 <div className="flex items-center gap-2 px-4">
                     <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
                     {date}
                 </div>
             </div>
