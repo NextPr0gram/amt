bb0ab073bb09738ae3f415cc3c3e6297110ea8b2
NextPr0gram <anafkarim@proton.me>
2025-02-20 16:14:47 +0000

    feat(frontend): add AddModuleModal component and integrate it with the dashboard, update dependencies

--- apps/frontend/app/dashboard/modules/page.tsx
+++ apps/frontend/app/dashboard/modules/page.tsx
@@ -3,6 +3,10 @@ import { DateStage } from "@/components/date-stage";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { DataTable } from "@/components/modules-page/data-table";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import ButtonWithModal from "../../../components/button-with-modal";
+import AddModuleModal from "../../../components/modules-page/add-module-modal";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
     // https://ui.shadcn.com/docs/components/data-table#basic-table
@@ -11,8 +15,8 @@ export default function Page() {
             <AppSidebar />
             <SidebarInset>
                 <DateStage />
-                <header className="flex h-16 shrink-0 items-center gap-2">
-                    <div className="flex items-center gap-2 px-4">
+                <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+                    <div className="flex items-center gap-2">
                         <Breadcrumb>
                             <BreadcrumbList>
                                 <BreadcrumbItem className="hidden md:block">
@@ -24,7 +28,18 @@ export default function Page() {
                                 </BreadcrumbItem>
                             </BreadcrumbList>
                         </Breadcrumb>
-                    </div>
+                    </div>{" "}
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button size="sm" className="ml-auto">
+                                Add module
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogTitle>Add module</DialogTitle>
+                            <AddModuleModal />
+                        </DialogContent>
+                    </Dialog>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
                     <DataTable />
