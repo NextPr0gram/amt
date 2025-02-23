ba4f38ab5d4bda0e0348c26c2e5e16f5e5e9e0ac
NextPr0gram <anafkarim@proton.me>
2025-02-23 23:34:28 +0000

    feat(frontend): move filter button closer to searchbar in DataTableToolbar

--- apps/frontend/components/data-table/data-table-toolbar.tsx
+++ apps/frontend/components/data-table/data-table-toolbar.tsx
@@ -20,7 +20,7 @@ export function DataTableToolbar<TData>({ table, filterColumn, filterTitle, filt
 
     return (
         <div className="flex items-center justify-between pb-4">
-            <div className="flex flex-1 items-center justify-between space-x-2">
+            <div className="flex flex-1 items-center space-x-2">
                 <Input placeholder={seachInputPlaceholder} value={table.getState().globalFilter ?? ""} onChange={(e: { target: { value: string } }) => table.setGlobalFilter(String(e.target.value))} className="max-w-sm" />
                 <div className="flex items-center space-x-2">
                     {table.getColumn(filterColumn) && <DataTableFacetedFilter column={table.getColumn(filterColumn)} title={filterTitle} options={filterOptions} customFacetMap={customFacetMap} />}
