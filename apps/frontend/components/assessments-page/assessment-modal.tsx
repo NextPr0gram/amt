cb1bd3527d0e804c177dbd3d31b6a668c66c8663
NextPr0gram <anafkarim@proton.me>
2025-03-03 12:57:10 +0000

    feat(backend): add validation for assessment input fields and add module field

--- apps/frontend/components/assessments-page/assessment-modal.tsx
+++ apps/frontend/components/assessments-page/assessment-modal.tsx
@@ -13,21 +13,11 @@ import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, Command
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Check, CheckCircle2, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Module, useModules } from "@/components/modules-page/module-context";
 import { DialogClose, DialogTitle } from "../ui/dialog";
 import { Alert, AlertDescription } from "../ui/alert";
 import MultiSelect from "../multi-select";
 import ModalAlert from "../modal-alert";
-
-export type ModuleTutor = {
-    id: number;
-    name: string;
-};
-
-type Year = {
-    id: number;
-    name: string;
-};
+import { Module } from "../modules-page/module-context";
 
 // module prop required if mode is edit
 interface ModuleModalProps {
@@ -35,60 +25,32 @@ interface ModuleModalProps {
     moduleId?: number;
 }
 
-const formSchema = z
-    .object({
-        moduleCode: z
-            .string()
-            .min(1, {
-                message: "This field is required",
-            })
-            .refine((s) => !s.includes(" "), "Module code cannot have spaces"),
-        moduleName: z.string().min(1, {
-            message: "This field is required",
-        }),
-        yearId: z
-            .number({
-                required_error: "Please select a year",
-            })
-            .int(),
-        moduleTutorId: z.number({ invalid_type_error: "Value must be an integer", message: "This field is required" }).int(), // represents module lead id
-        // module tutors must be array of numbers and the modulettorid cannot be in the array
-        moduleTutors: z.array(z.number({ invalid_type_error: "Values must be integers" })).optional(),
-    })
-    .refine(
-        (data) => {
-            if (!data.moduleTutors) return true; // If undefined, skip validation
-            if (data.moduleTutorId === undefined) return true; // If no module lead, skip validation
-            return !data.moduleTutors.includes(data.moduleTutorId);
-        },
-        { message: "Module tutors list cannot contain the module lead", path: ["moduleTutors"] }
-    );
+const formSchema = z.object({
+    moduleId: z.number().int(),
+    type: z.number().int(),
+    category: z.number().int(),
+    weight: z.number().int(),
+    releaseDate: z.string().optional(),
+    submissionDate: z.string().optional(),
+    durationInMinutes: z.number().int(),
+});
 
-const AssessmentModal = ({ type, moduleId }: ModuleModalProps) => {
-    const { fetchModules, modules } = useModules();
-    const [moduleTutors, setModuleTutors] = useState<ModuleTutor[]>([]);
-    const [years, setyears] = useState<Year[]>([]);
-    const [isModuleTutorPopoverOpen, setIsModuleTutorPopoverOpen] = useState(false);
-    const [isYearPopoverOpen, setIsYearPopoverOpen] = useState(false);
+const AssessmentModal = ({ type, assessmentId }: ModuleModalProps) => {
+    const [modules, setModules] = useState<Module[]>([]);
+    const [assessmentTypes, setAssessmentTypes] = useState([]);
+    const [assessmentCategories, setAssessmentCategories] = useState([]);
+    const [isModulePopoverOpen, setIsModulePopoverOpen] = useState(false);
     const [isEditing, setIsEditing] = useState(type === "add" ? true : false);
     const [showSuccess, setShowSuccess] = useState(false);
     const [showError, setShowError] = useState(false);
 
     useEffect(() => {
-        const fetchModuleTutors = async () => {
-            const res = await protectedFetch("/users", "GET");
-            const moduleTutors = res.data.map((moduleTutor: { id: number; firstName: string; lastName: string }) => ({ ...moduleTutor, name: moduleTutor.firstName + " " + moduleTutor.lastName }));
-            setModuleTutors(moduleTutors);
+        const fetchModules = async () => {
+            const res = await protectedFetch("/modules", "GET");
+            setModules(res.data);
         };
-
-        const fetchYears = async () => {
-            const res = await protectedFetch("/years", "GET");
-            const years = res.data.map((year: { id: number; name: string }) => year);
-            setyears(years);
-        };
-        fetchModuleTutors();
-        fetchYears();
-    }, []);
+        fetchModules();
+    }, [setModules]);
 
     useEffect(() => {
         let timer: NodeJS.Timeout;
@@ -113,21 +75,25 @@ const AssessmentModal = ({ type, moduleId }: ModuleModalProps) => {
     const getFormSchema = () => {
         if (type === "add") {
             return {
-                moduleCode: "",
-                moduleName: "",
-                yearId: undefined,
-                moduleTutorId: undefined,
-                moduleTutors: [],
+                moduleId: undefined,
+                typeId: undefined,
+                categoryId: undefined,
+                weight: undefined,
+                releaseDate: "",
+                submissionDate: "",
+                durationInMinutes: undefined,
             };
         } else if (type === "viewOrEdit") {
             const currentModule = modules.find((module: Module) => module.id === moduleId);
 
             return {
-                moduleCode: currentModule?.code || "",
-                moduleName: currentModule?.name || "",
-                yearId: currentModule?.yearId || undefined,
-                moduleTutorId: currentModule?.leadId || undefined,
-                moduleTutors: currentModule?.moduleTutorIds || [],
+                moduleId: undefined,
+                typeId: undefined,
+                categoryId: undefined,
+                weight: undefined,
+                releaseDate: "",
+                submissionDate: "",
+                durationInMinutes: undefined,
             };
         }
     };
@@ -226,92 +192,22 @@ const AssessmentModal = ({ type, moduleId }: ModuleModalProps) => {
     };
     return (
         <>
-            <DialogTitle>{type === "add" ? "Add new module" : isEditing ? "Edit module information" : "View module information"}</DialogTitle>
-            {showSuccess && <ModalAlert type="success" message={type === "add" ? "Module added successfully" : "Module updated successfully"} />}
+            <DialogTitle>{type === "add" ? "Add new assessment" : isEditing ? "Edit assessment information" : "View assessment information"}</DialogTitle>
+            {showSuccess && <ModalAlert type="success" message={type === "add" ? "Assessment created successfully" : "Assessment updated successfully"} />}
             {showError && <ModalAlert type="error" message="Something went wrong" />}
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                     <FormField
                         control={form.control}
-                        name="moduleCode"
-                        render={({ field }) => (
-                            <FormItem className={cn(!isEditing && "pointer-events-none")}>
-                                <FormLabel>Module Code</FormLabel>
-                                <FormControl>
-                                    <Input {...field} placeholder="e.g AB1CDE" readOnly={!isEditing} tabIndex={isEditing ? 0 : -1} />
-                                </FormControl>
-                                {/*<FormDescription>This is your public display name.</FormDescription>*/}
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="moduleName"
-                        render={({ field }) => (
-                            <FormItem className={cn(!isEditing && "pointer-events-none")}>
-                                <FormLabel>Module Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="e.g. System Design" {...field} readOnly={!isEditing} tabIndex={isEditing ? 0 : -1} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="yearId"
+                        name="moduleId"
                         render={({ field }) => (
                             <FormItem className={cn("flex flex-col", !isEditing && "pointer-events-none")}>
-                                <FormLabel>Year</FormLabel>
-                                <Popover open={isYearPopoverOpen} onOpenChange={setIsYearPopoverOpen}>
+                                <FormLabel>Module</FormLabel>
+                                <Popover open={isModulePopoverOpen} onOpenChange={setIsModulePopoverOpen}>
                                     <PopoverTrigger asChild>
                                         <FormControl>
                                             <Button size="sm" variant="outline" role="combobox" className={cn("justify-between font-normal", !field.value && "text-muted-foreground")} tabIndex={isEditing ? 0 : -1}>
-                                                {field.value ? `${years.find((year: Year) => year.id === field.value)?.name}` : "Select year"}
-                                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                            </Button>
-                                        </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0 -mt-2">
-                                        <Command>
-                                            <CommandList>
-                                                <CommandEmpty>No module tutors found.</CommandEmpty>
-                                                <CommandGroup>
-                                                    {isEditing &&
-                                                        years.map((year: Year) => (
-                                                            <CommandItem
-                                                                value={year.name}
-                                                                key={year.id}
-                                                                onSelect={() => {
-                                                                    form.setValue("yearId", year.id);
-                                                                    setIsYearPopoverOpen(false);
-                                                                }}
-                                                            >
-                                                                {year.name}
-                                                                <Check className={cn("ml-auto", year.id === field.value ? "opacity-100" : "opacity-0")} />
-                                                            </CommandItem>
-                                                        ))}
-                                                </CommandGroup>
-                                            </CommandList>
-                                        </Command>
-                                    </PopoverContent>
-                                </Popover>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="moduleTutorId"
-                        render={({ field }) => (
-                            <FormItem className={cn("flex flex-col", !isEditing && "pointer-events-none")}>
-                                <FormLabel>Module Lead</FormLabel>
-                                <Popover open={isModuleTutorPopoverOpen} onOpenChange={setIsModuleTutorPopoverOpen}>
-                                    <PopoverTrigger asChild>
-                                        <FormControl>
-                                            <Button size="sm" variant="outline" role="combobox" className={cn("justify-between font-normal", !field.value && "text-muted-foreground")} tabIndex={isEditing ? 0 : -1}>
-                                                {field.value ? `${moduleTutors.find((moduleTutor: ModuleTutor) => moduleTutor.id === field.value)?.name}` : "Assign module lead"}
+                                                {field.value ? `${modules.find((module) => module.id === field.value)?.code} - ${modules.find((module) => module.id === field.value)?.name}` : "Select module"}
                                                 <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                                             </Button>
                                         </FormControl>
@@ -323,17 +219,17 @@ const AssessmentModal = ({ type, moduleId }: ModuleModalProps) => {
                                                 <CommandEmpty>No module tutors found.</CommandEmpty>
                                                 <CommandGroup>
                                                     {isEditing &&
-                                                        moduleTutors.map((moduleTutor: ModuleTutor) => (
+                                                        modules.map((module) => (
                                                             <CommandItem
-                                                                value={moduleTutor.name}
-                                                                key={moduleTutor.id}
+                                                                key={module.id}
                                                                 onSelect={() => {
-                                                                    form.setValue("moduleTutorId", moduleTutor.id);
-                                                                    setIsModuleTutorPopoverOpen(false);
+                                                                    form.setValue("moduleId", module.id);
+                                                                    setIsModulePopoverOpen(false);
                                                                 }}
+                                                                className="cursor-pointer"
                                                             >
-                                                                {moduleTutor.name}
-                                                                <Check className={cn("ml-auto", moduleTutor.id === field.value ? "opacity-100" : "opacity-0")} />
+                                                                {`${module.code} - ${module.name}`}
+                                                                <Check className={cn("ml-auto", module.id === field.value ? "opacity-100" : "opacity-0")} />
                                                             </CommandItem>
                                                         ))}
                                                 </CommandGroup>
@@ -341,17 +237,6 @@ const AssessmentModal = ({ type, moduleId }: ModuleModalProps) => {
                                         </Command>
                                     </PopoverContent>
                                 </Popover>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="moduleTutors"
-                        render={({ field }) => (
-                            <FormItem className={cn("flex flex-col", !isEditing && "pointer-events-none")}>
-                                <FormLabel>Module Tutors</FormLabel>
-                                <MultiSelect data={moduleTutors} field={{ ...field, value: field.value || [] }} isEditing={isEditing} />
                                 <FormMessage />
                             </FormItem>
                         )}
