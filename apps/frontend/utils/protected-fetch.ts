79b633faf51406a937a3898f2e4104459792e89c
NextPr0gram <anafkarim@proton.me>
2025-03-16 17:06:10 +0000

    feat(frontend): enhance protectedFetch with error handling and JSON parsing

--- apps/frontend/utils/protected-fetch.ts
+++ apps/frontend/utils/protected-fetch.ts
@@ -1,5 +1,7 @@
 "use client";
 
+import { toast } from "sonner";
+
 // Wrapper function for fetch that handles token refresh
 export const protectedFetch = async (path: string, method: "GET" | "POST" | "PATCH" | "DELETE", body?: object) => {
     const endpoint = process.env.NEXT_PUBLIC_API_URL + path;
@@ -11,16 +13,34 @@ export const protectedFetch = async (path: string, method: "GET" | "POST" | "PAT
         credentials: "include" as RequestCredentials,
         body: body ? JSON.stringify(body) : undefined,
     };
-
+    const parseJson = async (res: Response) => {
+        const text = await res.text()
+        try {
+            const json = JSON.parse(text)
+            return json
+        } catch (err) {
+        }
+    }
     const fetchWithToken = async () => {
-        return await fetch(endpoint, opts);
-    };
+        try {
+            const res = await fetch(endpoint, opts);
+            return res;
+        } catch (err) {
+            toast.error("Network error", {
+                description: "Network error occurred while making the request"
+            })
+            return { status: 500, text: () => Promise.resolve("") } as Response;
 
+
+        }
+
+    };
     let res = await fetchWithToken();
-    let resJson = await res.json();
+    let resJson = await parseJson(res)
+
 
     // If access token expired then get refresh token
-    if (res.status !== 200 && resJson.errorCode === "InvalidAccessToken") {
+    if (res.status !== 200 && resJson && resJson.errorCode === "InvalidAccessToken") {
         const refreshRes = await fetch(process.env.NEXT_PUBLIC_API_URL + "/auth/refresh", {
             method: "GET",
             headers: {
@@ -31,7 +51,7 @@ export const protectedFetch = async (path: string, method: "GET" | "POST" | "PAT
         if (refreshRes.status === 200) {
             // Retry the original request with the new token
             res = await fetchWithToken();
-            resJson = await res.json();
+            resJson = await parseJson(res)
         } else {
             window.location.href = "/login"; // Redirect to login page
         }
