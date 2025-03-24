d105185c61c3c54b9f46a20bedefddd9bdc263fb
NextPr0gram <anafkarim@proton.me>
2025-03-24 20:35:11 +0000

    feat(backend): update user retrieval to limit what data is fetched for safety

--- apps/backend/controllers/user-controller.ts
+++ apps/backend/controllers/user-controller.ts
@@ -8,6 +8,12 @@ export const getUserHandler = catchErrors(async (req, res) => {
         where: {
             id: req.userId,
         },
+        select: {
+            id: true,
+            email: true,
+            firstName: true,
+            lastName: true,
+        },
     });
 
     appAssert(user, NOT_FOUND, "User not found");
