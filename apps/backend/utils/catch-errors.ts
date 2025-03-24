99bf12f4bd08e42fd1852bcd298bcfb997e30c2b
NextPr0gram <anafkarim@proton.me>
2025-03-24 19:05:43 +0000

    feat(backend): add safeExecute utility for error handling in async functions that are not in controllers

--- apps/backend/utils/catch-errors.ts
+++ apps/backend/utils/catch-errors.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { logMsg, logType } from "./logger";
 
-type AsyncController = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+type AsyncController = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => Promise<any>;
 
 // Catch errors in async controllers and pass them to the error handler middleware
-const catchErrors = (controller: AsyncController): AsyncController => {
+export const catchErrors = (controller: AsyncController): AsyncController => {
     return async (req, res, next) => {
         try {
             await controller(req, res, next);
@@ -13,4 +18,16 @@ const catchErrors = (controller: AsyncController): AsyncController => {
     };
 };
 
-export default catchErrors;
+type AsyncFunction<T> = () => Promise<T>;
+
+export const safeExecute = async <T>(
+    fn: AsyncFunction<T>,
+    errorMessage: string,
+): Promise<T | null> => {
+    try {
+        return await fn();
+    } catch (error: any) {
+        logMsg(logType.ERROR, `${errorMessage}: ${error.message}`);
+        return null;
+    }
+};
