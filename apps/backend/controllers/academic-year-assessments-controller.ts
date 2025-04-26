56241f941bc1aa76a5f70754c474778f85ea7417
NextPr0gram <anafkarim@proton.me>
2025-04-26 04:25:07 +0100

    feat(backend): add handlers for current academic year exams for TP1 and TP2

--- apps/backend/controllers/academic-year-assessments-controller.ts
+++ apps/backend/controllers/academic-year-assessments-controller.ts
@@ -12,7 +12,7 @@ import AppErrorCode from "../constants/app-error-code";
 export const getAcademicAssessmentsHandler = catchErrors(async (req, res) => {
     return res.status(OK).json();
 });
-export const getCurrentACYearExams = catchErrors(async (req, res) => {
+export const getCurrentACYearExamsHandler = catchErrors(async (req, res) => {
     const isModerationPhase4 = await prisma.moderationStatus.findFirst({
         select: {
             moderationPhaseId: true,
@@ -25,7 +25,7 @@ export const getCurrentACYearExams = catchErrors(async (req, res) => {
             code: AppErrorCode.NotExternalReview,
         });
     }
-    const userId = 38;
+    const userId = getUserIdFromToken(req.cookies.accessToken);
     appAssert(userId, BAD_REQUEST, "User ID not found in request");
 
     const currentAcademicYear = await getCurrentAcademicYear();
@@ -41,6 +41,82 @@ export const getCurrentACYearExams = catchErrors(async (req, res) => {
             academicYearId: currentAcademicYear.id,
             assessmentCategoryId: 9,
             sentToEr: false,
+        },
+    });
+
+    appAssert(currentAcademicYearExams.length > 0, NOT_FOUND, "Exam assessments not found for the current academic year");
+
+    return res.status(OK).json(currentAcademicYearExams);
+});
+
+export const getCurrentACYearExamsTp1Handler = catchErrors(async (req, res) => {
+    const isModerationPhase4 = await prisma.moderationStatus.findFirst({
+        select: {
+            moderationPhaseId: true,
+        },
+    });
+    appAssert(isModerationPhase4, NOT_FOUND, "Could not retrieve assessments becuse it is not external review phase yet");
+    if (!(isModerationPhase4.moderationPhaseId >= 4)) {
+        return res.status(INTERNAL_SERVER_ERROR).json({
+            message: "Canot retrieve assessments because it is not external review phase yet",
+            code: AppErrorCode.NotExternalReview,
+        });
+    }
+    const userId = getUserIdFromToken(req.cookies.accessToken);
+    appAssert(userId, BAD_REQUEST, "User ID not found in request");
+
+    const currentAcademicYear = await getCurrentAcademicYear();
+    appAssert(currentAcademicYear, INTERNAL_SERVER_ERROR, "Could not determine the current academic year");
+
+    const currentAcademicYearExams = await prisma.academicYearAssessment.findMany({
+        select: {
+            id: true,
+            name: true,
+            folderId: true,
+        },
+        where: {
+            academicYearId: currentAcademicYear.id,
+            assessmentCategoryId: 9,
+            sentToEr: false,
+            tpId: 1,
+        },
+    });
+
+    appAssert(currentAcademicYearExams.length > 0, NOT_FOUND, "Exam assessments not found for the current academic year");
+
+    return res.status(OK).json(currentAcademicYearExams);
+});
+
+export const getCurrentACYearExamsTp2Handler = catchErrors(async (req, res) => {
+    const isModerationPhase4 = await prisma.moderationStatus.findFirst({
+        select: {
+            moderationPhaseId: true,
+        },
+    });
+    appAssert(isModerationPhase4, NOT_FOUND, "Could not retrieve assessments becuse it is not external review phase yet");
+    if (!(isModerationPhase4.moderationPhaseId >= 4)) {
+        return res.status(INTERNAL_SERVER_ERROR).json({
+            message: "Canot retrieve assessments because it is not external review phase yet",
+            code: AppErrorCode.NotExternalReview,
+        });
+    }
+    const userId = getUserIdFromToken(req.cookies.accessToken);
+    appAssert(userId, BAD_REQUEST, "User ID not found in request");
+
+    const currentAcademicYear = await getCurrentAcademicYear();
+    appAssert(currentAcademicYear, INTERNAL_SERVER_ERROR, "Could not determine the current academic year");
+
+    const currentAcademicYearExams = await prisma.academicYearAssessment.findMany({
+        select: {
+            id: true,
+            name: true,
+            folderId: true,
+        },
+        where: {
+            academicYearId: currentAcademicYear.id,
+            assessmentCategoryId: 9,
+            sentToEr: false,
+            tpId: 2,
         },
     });
 
